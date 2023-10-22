bb7cf14f325b2232cfe7d06d4feeccf546bb15d6
justabayet <anthony.bayet@hotmail.com>
2023-10-22 17:48:58 +0200

    # Cleanup archives

--- src/archive/WallTargetable.ts
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three'
-
-
-export default class WallTargetable extends THREE.Mesh {
-
-  target: THREE.Mesh = null
-  axis: THREE.Vector3 = new THREE.Vector3(0, 0, 0)
-}
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,3 @@
-
-
 export interface Updatable {
   update: () => void
 }
